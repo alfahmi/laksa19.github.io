96d689e45c6640b33fc01354806c6361ec0acd7b
Laksamadi Guko <elru.lakusa@gmail.com>
2019-09-14 15:33:53 +0700

    Create autowa.user.js

---
+++ autowa/autowa.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Auto Click WA
+// @namespace    https://laksa19.github.io/autowa
+// @updateURL    https://raw.githubusercontent.com/laksa19/autowa/master/autowa.user.js
+// @downloadURL  https://raw.githubusercontent.com/laksa19/autowa/master/autowa.user.js
+// @version      0.0.20
+// @author       Laksamadi Guko
+// @description  Auto Click For Whatsapp Web
+// @match        https://web.whatsapp.com/*
+// @grant        none
+// ==/UserScript==
+
+var _0x637d=["\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x67\x75\x6B\x6F","\x67\x65\x74","\x73\x65\x61\x72\x63\x68\x50\x61\x72\x61\x6D\x73","\x6A\x6F\x73\x73","\x62\x75\x74\x74\x6F\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x63\x6C\x69\x63\x6B","\x64\x69\x76","\x6C\x65\x6E\x67\x74\x68","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x4F\x4B","\x63\x6C\x6F\x73\x65"];(function(){_0x637d[0];var _0x7ca9x1= new URL(window[_0x637d[1]]);var _0x7ca9x2=_0x7ca9x1[_0x637d[4]][_0x637d[3]](_0x637d[2]);if(_0x7ca9x2== _0x637d[5]){var _0x7ca9x3=setInterval(function(){if(document[_0x637d[7]](_0x637d[6])[5]){document[_0x637d[7]](_0x637d[6])[5][_0x637d[8]]();_0x7ca9x6()}},2000);setInterval(function(){var _0x7ca9x4=document[_0x637d[7]](_0x637d[9]);for(var _0x7ca9x5=0;_0x7ca9x5< _0x7ca9x4[_0x637d[10]];_0x7ca9x5++){if((_0x7ca9x4[_0x7ca9x5][_0x637d[11]])== _0x637d[12]){_0x7ca9x4[_0x7ca9x5][_0x637d[8]]();_0x7ca9x6()}}},2000);function _0x7ca9x6(){setTimeout(function(){window[_0x637d[13]]()},3500)}}})()
